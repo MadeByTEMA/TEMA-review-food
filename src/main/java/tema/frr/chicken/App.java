6f879dd1f7de178063b80015c9bc42812368cc15
TEMA <tema_@naver.com>
2019-12-18 16:03:56 +0900

    프로젝트 생성

---
+++ src/main/java/tema/frr/chicken/App.java
@@ -0,0 +1,12 @@
+// 먹어봐따 Try it(가칭) : 음식 후기 및 평점 사이트
+// 실제 프렌차이즈에 다양한 음식 먹은 후기와 별점들을 유저들이 남기고,
+// 해당 프렌차이즈에서 뭘 먹을지 제시, 추천해주는 사이트.
+//
+package tema.frr.chicken;
+
+public class App {
+    public static void main(String[] args) {
+
+      System.out.println("먹어봐따에 오신걸 환영합니다.");
+    }
+}
