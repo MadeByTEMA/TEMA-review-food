f23f40a85b1bb57dc8fead12a230dfa3115cd00b
TEMA <tema_@naver.com>
2020-01-16 18:43:23 +0900

    README v024_2 적용

--- src/main/java/tema/frr/util/Queue.java
+++ src/main/java/tema/frr/util/Queue.java
@@ -23,4 +23,25 @@ public class Queue<E> extends LinkedList<E> implements Cloneable{
   public Iterator<E> iterator() {
     return new QueueIterator<E>(this);
   }
+  
+  public class QueueIterator<E> implements Iterator<E>{
+    
+    Queue<E> queue;
+    
+    public QueueIterator(Queue<E> queue) {
+      this.queue = queue.clone();
+    }
+    
+    @Override
+    public boolean hasNext() {
+      return queue.size > 0;
+    }
+
+    @Override
+    public E next() {
+      // TODO Auto-generated method stub
+      return queue.get(0);
+    }
+    
+  }
 }
--- src/main/java/tema/frr/util/Stack.java
+++ src/main/java/tema/frr/util/Stack.java
@@ -77,4 +77,25 @@ public class Stack<E> implements Cloneable {
   public Iterator<E> iterator() {
     return new StackIterator<E>(this);
   }
+  
+  public class StackIterator<E> implements Iterator<E> {
+
+    Stack<E> stack;
+    
+    public StackIterator(Stack<E> stack) {
+      this.stack = stack.clone();
+    }
+    
+     @Override
+    public boolean hasNext() {
+      // TODO Auto-generated method stub
+      return !stack.empty();
+    }
+
+    @Override
+    public E next() {
+      // TODO Auto-generated method stub
+      return stack.pop();
+    }
+  }
 }
